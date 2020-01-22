3fb04df6962ee0ec3255dc6fb7c725fbd0fc4068
gy741 <gy741.kim@gmail.com>
2020-01-22 19:04:26 +0900

    Update bugbounty_tools_installer.sh

--- bugbounty_tools_installer.sh
+++ bugbounty_tools_installer.sh
@@ -1,1 +1,26 @@
+#!/usr/bin/env bash
+
+TOOLS="$HOME/bounty/tools";
+
+#기본환경 구성
 sudo apt-get install git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full unzip -y;
+
+mkdir -pv "$HOME"/bounty/tools;
+
+# go 언어설치
+wget -nv https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz;
+sudo tar -C /usr/local -xzf go1.13.6.linux-amd64.tar.gz;
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:" >> "$HOME"/.profile;
+echo "export GOPATH=$HOME/go" >> "$HOME"/.profile;
+source "$HOME"/.profile;
+rm -rf go1.13.6.linux-amd64.tar.gz;
+
+# amass 설치
+wget -nv https://github.com/OWASP/Amass/releases/download/v3.4.2/amass_v3.4.2_linux_amd64.zip -O "$TOOLS"/amass.zip;
+unzip -j  "$TOOLS"/amass.zip -d "$TOOLS"/amass;
+rm "$TOOLS"/amass.zip;
+
+# subfinder 설치
+wget -nv https://github.com/projectdiscovery/subfinder/releases/download/v2.3.0/subfinder-linux-amd64.tar -O "$TOOLS"/subfinder.zip;
+unzip -j  "$TOOLS"/subfinder.zip -d "$TOOLS"/subfinder;
+rm "$TOOLS"/subfinder.zip;
