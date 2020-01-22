1f97cb7507543c9cb4ab19de98ccc74bfc484836
gy741 <gy741.kim@gmail.com>
2020-01-22 19:49:18 +0900

    Update bugbounty_tools_installer.sh

--- bugbounty_tools_installer.sh
+++ bugbounty_tools_installer.sh
@@ -4,7 +4,7 @@ TOOLS="$HOME/bounty/tools";
 
 #기본환경 구성
 sudo apt-get update;
-sudo apt-get install unzip git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full -y;
+sudo apt-get install unzip git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full python-requests python-dnspython python-argparse -y;
 
 mkdir -pv "$HOME"/bounty/tools;
 
@@ -25,5 +25,19 @@ rm "$TOOLS"/amass.zip;
 mkdir -pv "$HOME"/bounty/tools/subfinder;
 wget -nv https://github.com/projectdiscovery/subfinder/releases/download/v2.3.0/subfinder-linux-amd64.tar -O "$TOOLS"/subfinder.tar;
 sudo tar -C "$TOOLS"/subfinder -xzf "$TOOLS"/subfinder.tar;
-mv "$TOOLS"/subfinder-linux-amd64 "$TOOLS"/subfinder
+mv "$TOOLS"/subfinder/subfinder-linux-amd64 "$TOOLS"/subfinder/subfinder
 rm "$TOOLS"/subfinder.tar;
+
+# Sublist3r 설치
+git clone https://github.com/aboul3la/Sublist3r.git "$TOOLS"/Sublist3r;
+cd "$TOOLS"/Sublist3r;
+sudo pip install -r requirements.txt;
+
+# knock 설치
+git clone https://github.com/guelfoweb/knock "$TOOLS"/knock;
+cd "$TOOLS"/knock;
+sudo python setup.py install;
+
+# goaltdns 설치
+source $HOME/.profile;
+go get -u github.com/subfinder/goaltdns;
